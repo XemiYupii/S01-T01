5d57dcc475273adf9bd616d1646c3571ab9f1d4d
XemiYupii <xemi86@gmail.com>
2023-01-26 11:47:37 +0100

    modificacio class Cotxe

--- exercici2/Cotxe.java
+++ exercici2/Cotxe.java
@@ -37,19 +37,4 @@ public class Cotxe {
 		return MARCA +", " +model + ", " +POTENCIA;
 	}
 	
-	/*L'atribut static final MARCA l'hem d'inicialitzar en el moment que el declarem, el propi IDE ja ens ho fa saber, al ser estàtic és un atribut de la classe i és una constant, amb lo kual
-	 no podem fer un setter per modificar-lo.
-	 L'atribut static model, també fa referència a la classe, el podriem inicialitzar en el constructor, però cada vegda k instanciessim un nou objcte amb akst atribut canviarem el model
-	 dels altres objectes anteriors creats. Crec k el més lògic seria instanciar-lo en el moment k el declarem.
-	 L'atribut potencia el podem instanciar en el constructor, al ser una constant tampoc es podrà modificar ni crear un setter.
-	 El metode frenar al ser estàtic és un metode de la classe per tant per invocar-lo fem servir el nom de la classe . i el nom del metode. No fa falta instanciar cap objecte.
-	 El metode accelerar és un metode del propi objecte per tant per invocar-lo abans hem de crear akst objecte.
-	 */
-	
-
-	
-
-	
-	
-
 }
